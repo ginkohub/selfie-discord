e21bcfd54b2fa9122058c5a8fcee24ccfe8a2eab
ginkohub <ginkohub@gmail.com>
2026-06-06 08:45:09 +0800

    docs: we need to commit all files just for github username ?

--- src/wiki.js
+++ src/wiki.js
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/
  *
- * This code is part of Ginko project (https://github.com)
+ * This code is part of Ginko project (https://github.com/ginkohub)
  */
 
 export async function searchWiki(query) {
