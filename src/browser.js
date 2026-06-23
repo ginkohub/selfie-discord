0d6a6f9ff421e890c43404d1d9a579cc4c99189a
ginkohub <ginkohub@gmail.com>
2026-06-23 11:40:04 +0800

    lint: formatting code with biome, ahh we're tired of editor vs biome fighting

--- src/browser.js
+++ src/browser.js
@@ -677,7 +677,7 @@ async function downloadCapCut(url) {
               };
             }
           }
-        } catch { }
+        } catch {}
       }
     }
 
@@ -813,8 +813,8 @@ export async function download(url) {
     const urlLower = (mediaUrl || "").toLowerCase();
     mediaType =
       urlLower.includes(".mp4") ||
-        urlLower.includes("/video/") ||
-        urlLower.includes(".m3u8")
+      urlLower.includes("/video/") ||
+      urlLower.includes(".m3u8")
         ? "video"
         : "image";
   } else if (/capcut\.com/.test(url)) {
